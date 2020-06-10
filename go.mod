261b64111b842e8fe0e92ed66f221c6517183471
el-Joft <ottimothy@gmail.com>
2020-06-10 16:51:13 +0100

    feat(setup) application initial setup

---
+++ go.mod
@@ -0,0 +1,13 @@
+module coffee-mate
+
+go 1.14
+
+require (
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.12
+	github.com/joho/godotenv v1.3.0
+	github.com/jpoles1/gopherbadger v2.2.0+incompatible
+	github.com/mitchellh/mapstructure v1.3.2
+	github.com/saefullohmaslul/golang-example v0.0.0-20200517165016-0b7230ac3130
+)
---
+++ src/middleware/exception/error_response_test.go
@@ -0,0 +1,18 @@
+package exception_test
+
+import (
+	"coffee-mate/src/middlewares/exception"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInternalServerError(t *testing.T) {
+	var errors []map[string]interface{}
+	defer func() {
+		if err := recover(); err != nil {
+			assert.NotEmpty(t, err)
+		}
+	}()
+	exception.InternalServerError("ISE Testing", errors)
+}
